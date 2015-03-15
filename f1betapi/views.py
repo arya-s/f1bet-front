79964ea3c87c4fb5d02a39d34001119245421b19
arya-s <andrei.borza@gmail.com>
2015-03-16 04:16:08 +0900

    Data decoding.

--- f1betapi/views.py
+++ f1betapi/views.py
@@ -2,12 +2,18 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.http import Http404
 
+import json
+
 @csrf_exempt
 def index(request):
 
     if request.method == 'POST':
 
-        print request.body
+        data = json.loads(request.body)
+
+        print data
+
+
         return JsonResponse({ 'driver': 'vettel' })
 
     return Http404
