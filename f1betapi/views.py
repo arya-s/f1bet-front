302bea6bf47ddd7225f57379695ef9b741899a2b
arya-s <andrei.borza@gmail.com>
2015-03-16 02:11:42 +0900

    XHR still not working.

--- f1betapi/views.py
+++ f1betapi/views.py
@@ -5,17 +5,4 @@ from django.http import Http404
 @csrf_exempt
 def index(request):
 
-    response = dict()
-
-    payload = request.POST.get('payload', None)
-    if payload is None:
-        raise Http404
-
-    data = payload.get('data', None)
-    if data:
-        driver = data.get('driver')
-
-        if driver == 'Vettel':
-            response['position'] = 3
-
-    return JsonResponse(response)
+    return JsonResponse(request)
