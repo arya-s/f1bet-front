1997735baadc885a7c680f828e4c4b05fbf340ec
arya-s <andrei.borza@gmail.com>
2015-03-16 04:41:13 +0900

    .

--- f1betapi/views.py
+++ f1betapi/views.py
@@ -9,8 +9,13 @@ def index(request):
 
     if request.method == 'POST':
 
-        print request
+        data = json.loads(request.body)
+        print data
 
-        return JsonResponse({ 'driver': 'vettel' })
+        if data.get('driver') == 'vettel'
+            return JsonResponse({ 'pos': '3' })
+        else
+            return JsonResponse({ 'data': 'unkown driver'})
+
 
     return Http404
