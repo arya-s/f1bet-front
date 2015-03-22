dd3853c96968e77237f78059b20b5683f04fcda9
arya-s <andrei.borza@gmail.com>
2015-03-22 20:59:49 +0900

    Forgot to import Admin

--- f1bet/urls.py
+++ f1bet/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from f1betcore.models import RedditUser, Team, Driver, DriverShort, Race, Bet
 from rest_framework import serializers, viewsets, routers
 
